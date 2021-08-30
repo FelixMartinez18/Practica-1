47738dcb060dc01fd80cf79b1fae5b15efe06581
derek <derek@FELIXMARTINEZ>
2021-08-29 23:40:51 -0500

    Agregar archivos de proyecto.

---
+++ Practica 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4341;
+            int b = 4894;
+            int c = a - b;
+            Console.WriteLine(c);
+        }
+    }
+}
